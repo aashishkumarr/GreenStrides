ce1bec2157a1b4f6dbdc0107c1eb7a0374ed3d36
Aashish Kumar <aashish14002@iiitd.ac.in>
2021-04-07 01:50:56 +0200

    Add tab layout

---
+++ app/src/main/java/com/example/greenstrides/Goals.kt
@@ -0,0 +1,16 @@
+package com.example.greenstrides
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+class Goals : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_goals, container, false)
+    }
+}
