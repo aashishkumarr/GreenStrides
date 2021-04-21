997021785ec8483540ac6c2ddf7491010bfde25d
Aashish Kumar <aashish14002@iiitd.ac.in>
2021-04-21 20:03:53 +0200

    Update the webview Urls Refactor and styling fix

--- app/src/main/java/com/example/greenstrides/UrlConstants.kt
+++ app/src/main/java/com/example/greenstrides/UrlConstants.kt
@@ -2,15 +2,15 @@ package com.example.greenstrides
 
 object UrlConstants {
     const val VISUALIZATION_VIEW =
-        "https://observablehq.com/embed/14ab270eb83c3ec2?cells=viewof+datasetSelector%2Cviewof+goal%2Cviewof+timeSelector%2Cbarchart%2Cviewof+monthSelector%2Cchart%2Cchart2"
+        "https://observablehq.com/embed/@s145005/green-strides?cells=viewof+goal%2CbarchartLabel%2Cviewof+timeSelector%2Cbarchart%2Cviewof+avatarChart_noNational%2CdonutChartLabel%2Cviewof+monthSelector%2Cchart%2Cchart2"
 
     const val COLLECT_DATA_VIEW =
-        "https://observablehq.com/embed/@aashish14002/radio-button-group-newline?cells=viewof+choiceOfCar"
+        "https://observablehq.com/embed/@s145005/green-strides?cells=viewof+choiceOfCar"
 
     const val GOALS_VIEW =
-        "https://observablehq.com/embed/14ab270eb83c3ec2?cells=viewof+datasetSelector%2Cviewof+goal%2Cviewof+timeSelector%2Cbarchart%2Cviewof+monthSelector%2Cchart%2Cchart2"
+        "https://observablehq.com/embed/@s145005/green-strides?cells=viewof+goal%2Cstat1%2Cstat2%2Cstat3"
 
     const val HOME_VIEW =
-        "https://observablehq.com/embed/@aashish14002/green-strides-test?cells=homeTab"
+        "https://observablehq.com/embed/@s145005/green-strides?cells=homeTab%2Chomestat1%2Chomestat2"
 
 }
