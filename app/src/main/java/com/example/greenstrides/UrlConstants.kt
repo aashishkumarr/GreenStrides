b8d6878307ee09145375f2c8f55aba2bdb200bd5
Aashish Kumar <aashish14002@iiitd.ac.in>
2021-05-09 22:12:14 +0200

    update observable notebook urls

--- app/src/main/java/com/example/greenstrides/UrlConstants.kt
+++ app/src/main/java/com/example/greenstrides/UrlConstants.kt
@@ -2,13 +2,12 @@ package com.example.greenstrides
 
 object UrlConstants {
     const val VISUALIZATION_VIEW =
-        "https://observablehq.com/embed/@s145005/green-strides?cells=viewof+goal%2CbarchartLabel%2Cviewof+timeSelector%2Cbarchart%2Cviewof+avatarChart_noNational%2CdonutChartLabel%2Cviewof+monthSelector%2Cchart%2Cchart2"
-
+        "https://observablehq.com/embed/@s145005/green-strides?cells=co2GoalString%2Cviewof+goal%2CbarchartLabel%2Cviewof+timeSelector%2Cbarchart%2CdonutChartLabel%2Cviewof+monthSelector%2CdonutChartLabel1%2Cchart%2CdonutChartLabel2%2Cchart2"
     const val COLLECT_DATA_VIEW =
         "https://observablehq.com/embed/@s145005/green-strides?cells=viewof+choiceOfCar"
 
     const val GOALS_VIEW =
-        "https://observablehq.com/embed/@s145005/green-strides?cells=viewof+goal%2Cstat1%2Cstat2%2Cstat3"
+        "https://observablehq.com/embed/@s145005/green-strides?cells=co2GoalString%2Cviewof+goal%2Cstat1%2Cstat2%2Cstat3"
 
     const val HOME_VIEW =
         "https://observablehq.com/embed/@s145005/green-strides?cells=homeTab%2Chomestat1%2Chomestat2"
